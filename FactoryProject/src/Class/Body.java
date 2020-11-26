6e770261850083457814d89bc2f958731d4ac42d
Simó Gergely <73936928+Cerpo@users.noreply.github.com>
2020-11-26 11:49:55 +0100

    Create Body.java

---
+++ FactoryProject/src/Class/Body.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+/**
+ *
+ * @author Cerpo
+ */
+public class Body extends Component {
+    
+
+    @Override
+    void setTypeCode(String typeCode) {
+        this.typeCode = typeCode;
+    }
+}
