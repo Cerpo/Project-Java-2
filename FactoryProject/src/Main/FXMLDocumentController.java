d0cb865c848dd11cf90185b7e4429a1e4b65fe55
kcsanad314 <61548482+kcsanad314@users.noreply.github.com>
2020-12-11 19:19:44 +0100

    Adding FXML parts

--- FactoryProject/src/Main/FXMLDocumentController.java
+++ FactoryProject/src/Main/FXMLDocumentController.java
@@ -10,7 +10,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TableView;
 
 /**
  *
@@ -18,10 +22,50 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private TableView<?> componentTable;
+    @FXML
+    private Button engineBtn;
+    @FXML
+    private Button wheelBtn;
+    @FXML
+    private Button electronicBtn;
+    @FXML
+    private Button bodyBtn;
+    @FXML
+    private Button carBtn;
+    @FXML
+    private ComboBox<String> engineList;
+    @FXML
+    private ComboBox<String> electronicList;
+    @FXML
+    private ComboBox<String> wheelList;
+    @FXML
+    private ComboBox<String> bodyList;
+    @FXML
+    private ComboBox<String> carList;
+    @FXML
+    private ProgressBar enginePB;
+    @FXML
+    private ProgressBar electronicPB;
+    @FXML
+    private ProgressBar carPB;
+    @FXML
+    private ProgressBar wheelPB;
+    @FXML
+    private ProgressBar bodyPB;
+    @FXML
+    private Label engineLabel;
+    @FXML
+    private Label wheelLabel;
+    @FXML
+    private Label electronicLabel;
+    @FXML
+    private Label bodyLabel;
+    @FXML
+    private Label carLabel;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
